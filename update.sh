ed375b8cc6fe4433aff60f242b3a36cbd6343704
dieWehmut <diesehnsucht0@gmail.com>
2026-05-15 11:13:31 +0800

    2026-05-15 11:13:29

--- update.sh
+++ update.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-git status
 git add .
 commit_info=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "$commit_info" || echo "Nothing to commit"
-git pull --rebase origin main
-git push origin main
+
+git push origin main --force
